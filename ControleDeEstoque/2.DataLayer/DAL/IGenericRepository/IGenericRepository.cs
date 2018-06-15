e60c29394ebad01459faa85ba3077430fb1955d0
Estefani Fujimoto <estefani.fujimoto@hotmail.com>
2018-06-15 01:48:50 -0300

    Implementação IGenericRepository e GenericRepository

---
+++ ControleDeEstoque/2.DataLayer/DAL/IGenericRepository/IGenericRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.IGenericRepository
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        void Adicionar(T entity);
+        IQueryable<T> BuscarTodos();
+        IQueryable<T> Buscar(Func<T, bool> predicate);
+        void Editar(T entity);
+        void Excluir(Expression<Func<T, bool>> predicate);
+        void Salvar();
+    }
+}
