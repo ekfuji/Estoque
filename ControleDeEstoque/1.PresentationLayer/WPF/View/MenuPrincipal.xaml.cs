72e28c33cc9f341b27230cf80b709e696e8041f1
Estefani Fujimoto <estefani.fujimoto@hotmail.com>
2018-06-15 01:10:30 -0300

    Criação das class libraries e das telas (MenuPrincipal, produto)

---
+++ ControleDeEstoque/1.PresentationLayer/WPF/View/MenuPrincipal.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using View;
+
+namespace View
+{
+    /// <summary>
+    /// Interação lógica para MainWindow.xam
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private WindowState lastNonMinimizedState = WindowState.Normal;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        
+        private void ButtonOpenMenu_Click(object sender, RoutedEventArgs e)
+        {
+            ButtonCloseMenu.Visibility = Visibility.Visible;
+            ButtonOpenMenu.Visibility = Visibility.Collapsed;
+        }
+
+        private void ButtonCloseMenu_Click(object sender, RoutedEventArgs e)
+        {
+            ButtonCloseMenu.Visibility = Visibility.Collapsed;
+            ButtonOpenMenu.Visibility = Visibility.Visible;
+        }
+
+        private void ListViewMenu_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UserControl usc = null;
+            GridMain.Children.Clear();
+            switch (((ListViewItem)((ListView)sender).SelectedItem).Name)
+            {
+                
+                case "ItemCreate":
+                    usc = new UserControlCreate();
+                    GridMain.Children.Add(usc);
+                    break;
+                case "ItemMovement":
+                    usc = new UserControlMoviment();
+                    GridMain.Children.Add(usc);
+                    break;
+                default:
+                    break;
+            } 
+        }
+
+        private void btnFechar_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnMinimizar_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            this.lastNonMinimizedState = this.WindowState;
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void ItemCreate_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+    }
+}
