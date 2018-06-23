942c160a59a7fc311e636d1c3b22dcda277d38e2
Estefani Fujimoto <estefani.fujimoto@hotmail.com>
2018-06-23 02:27:39 -0300

    Implementação das classes FuncionarioRepository e FuncionarioApplication

---
+++ ControleDeEstoque/DAL/RepositoryImplementation/FuncionarioRepository.cs
@@ -0,0 +1,14 @@
+﻿using DAL.GenericRepository;
+using DAL.ModeloDeDados;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.RepositoryImplementation
+{
+    public class FuncionarioRepository : GenericRepository<Funcionario>, IDisposable
+    {
+    }
+}
---
+++ ControleDeEstoque/Orientacao.Application/ApplicationImplementation/FuncionarioApplication.cs
@@ -0,0 +1,80 @@
+﻿using DAL.ModeloDeDados;
+using DAL.RepositoryImplementation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Orientacao.Application.ApplicationImplementation
+{
+    public class FuncionarioApplication
+    {
+        private readonly FuncionarioRepository FuncionarioRepository = new FuncionarioRepository();
+        public FuncionarioApplication()
+        {
+        }
+
+        public FuncionarioApplication(FuncionarioRepository funcionarioRepository)
+        {
+            FuncionarioRepository = funcionarioRepository;
+        }
+
+        public Funcionario BuscarFuncionario(Expression<Func<Funcionario, bool>> predicate)
+        {
+            return FuncionarioRepository.Buscar(predicate).FirstOrDefault();
+        }
+
+        public IList<Funcionario> BuscarPor(Expression<Func<Funcionario, bool>> predicate)
+        {
+            return FuncionarioRepository.BuscarPor(predicate).ToList();
+        }
+
+        public IList<Funcionario> BuscarTodos()
+        {
+            return FuncionarioRepository.BuscarTodos().ToList();
+        }
+
+        public string SalvarPFuncionario(Funcionario func)
+        {
+            var error = "";
+            try
+            {
+                if (func.idFuncionario== 0)
+                {
+                    FuncionarioRepository.Adicionar(func);
+                }
+                else
+                {
+                    FuncionarioRepository.Editar(func);
+                }
+
+                FuncionarioRepository.Salvar();
+            }
+            catch (Exception ex)
+            {
+                error = ex.Message;
+            }
+
+            return error;
+        }
+
+        public string ExcluirFuncionario(Funcionario func)
+        {
+            var error = "";
+
+            try
+            {
+                FuncionarioRepository.Excluir(func);
+                FuncionarioRepository.Salvar();
+            }
+            catch (Exception ex)
+            {
+                error = ex.Message;
+            }
+
+            return error;
+        }
+    }
+}
