3119af931271d4c42f30fff8410adae332b407f7
Estefani Fujimoto <estefani.fujimoto@hotmail.com>
2018-06-23 23:55:38 -0300

    Implementação das classes (UsuarioRepository ,UsuarioApplication) e Início da implementação da classe UserConnection para o login.

---
+++ ControleDeEstoque/DAL/UserConnection/UserConnection.cs
@@ -0,0 +1,24 @@
+﻿using DAL.ModeloDeDados;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.UserConnection
+{
+    class UserConnection
+    {
+        public bool Logar(string login, string senha)
+        {
+            var db = new EstoqueEntities();
+
+            var usuario = db.Usuario.FirstOrDefault(u => u.loginUsuario == login && u.senhaUsuario == senha);
+            if(usuario != null)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ ControleDeEstoque/Orientacao.Application/ApplicationImplementation/UsuarioApplication.cs
@@ -0,0 +1,84 @@
+﻿using DAL.ModeloDeDados;
+using DAL.RepositoryImplementation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Orientacao.Application.ApplicationImplementation
+{
+    public class UsuarioApplication
+    {
+        private readonly UsuarioRepository userRepository = new UsuarioRepository();
+
+        public UsuarioApplication()
+        {
+
+        }
+        public UsuarioApplication(UsuarioRepository usuarioRepository)
+        {
+            userRepository = usuarioRepository;
+        }
+
+        public Usuario BuscarUsuario(Expression<Func<Usuario, bool>> predicate)
+        {
+            return userRepository.Buscar(predicate).FirstOrDefault();
+        }
+
+        public IList<Usuario> BuscarPor(Expression<Func<Usuario, bool>> predicate)
+        {
+            return userRepository.BuscarPor(predicate).ToList();
+        }
+
+        public IList<Usuario> BuscarTodos()
+        {
+            return userRepository.BuscarTodos().ToList();
+        }
+
+        public string SalvarUsuario(Usuario usuario)
+        {
+            var error = "";
+            try
+            {
+
+                if (usuario.idUsuario == 0)
+                {
+                    userRepository.Adicionar(usuario);
+                }
+                else
+                {
+                    userRepository.Editar(usuario);
+                }
+
+
+
+                userRepository.Salvar();
+            }
+            catch (Exception ex)
+            {
+                error = ex.Message;
+            }
+
+            return error;
+        }
+
+        public string ExcluirUsuario(Usuario usuario)
+        {
+            var error = "";
+
+            try
+            {
+                userRepository.Excluir(usuario);
+                userRepository.Salvar();
+            }
+            catch (Exception ex)
+            {
+                error = ex.Message;
+            }
+
+            return error;
+        }
+    }
+}
